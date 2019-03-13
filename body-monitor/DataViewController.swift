86ed05ffd30b520d2080a38f3ccb748cbc0779c1
anarmawala <arshadn00@gmail.com>
2019-03-13 13:40:11 -0500

    Initial Commit

---
+++ body-monitor/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  body-monitor
+//
+//  Created by Arshad Narmawala on 3/13/19.
+//  Copyright © 2019 js. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
